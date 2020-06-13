9cee348c209492ef7c13254e54a8b2a40406bc0d
Abhijeet <abhijeetlokhande1996@gmail.com>
2020-06-13 16:19:48 +0530

    @angular/cdk/scrolling issue fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import "../polyfills";
 
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
@@ -13,7 +15,8 @@ import { TopNavBarComponent } from "./core/top-nav-bar/top-nav-bar.component";
 import { AutoFocusDirective } from "./directives/auto-focus.directive";
 import { MonthlySipTableComponent } from "./feature/monthly-sip/monthly-sip-table/monthly-sip-table.component";
 import { ChartsModule } from "ng2-charts";
-import { ChartComponent } from './shared/chart/chart.component';
+import { ChartComponent } from "./shared/chart/chart.component";
+import { TableModule } from "primeng/table";
 
 @NgModule({
   declarations: [
@@ -26,11 +29,13 @@ import { ChartComponent } from './shared/chart/chart.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
     ChartsModule,
+    TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
