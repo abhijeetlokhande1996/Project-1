d68c82b6655e1f10779ee88a8ea277952739d705
Abhijeet <abhijeetlokhande1996@gmail.com>
2020-06-16 19:50:05 +0530

    navModel model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component, OnInit } from "@angular/core";
+import { DatabaseService } from "./services/database.service";
+import { map } from "rxjs/operators";
+import { NavModel } from "./models/nav.model";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(private dbService: DatabaseService) {}
+  ngOnInit(): void {
+    this.dbService
+      .fetchLatestNAV()
+      .pipe(
+        map((data: Array<{}>) => {
+          const arrToReturn: Array<NavModel> = [];
+          const navObj = new NavModel();
+          for (const item of data) {
+            navObj.date = new Date(item["Date"]);
+            navObj.mutualFundFamily = item["Mutual Fund Family"];
+            navObj.netAssetValue = parseFloat(item["Net Asset Value"]);
+            navObj.schemeName = item["Scheme Name"];
+            navObj.schemeCode = parseInt(item["Scheme Code"]);
+            navObj.schemeType = item["Scheme Type"];
+            navObj.schemeCategory = item["Scheme Category"];
+            arrToReturn.push(JSON.parse(JSON.stringify(navObj)));
+          }
+          return arrToReturn;
+        })
+      )
+      .subscribe((resp: Array<NavModel>) => {
+        console.log(resp);
+      });
+  }
 }
