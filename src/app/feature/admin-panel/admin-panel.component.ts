1b8a8af04ec463b834e3c359c3be013336c25692
Abhijeet <abhijeetlokhande1996@gmail.com>
2020-06-18 01:22:09 +0530

    admin-panel component structure done | IAddScheme interface added

--- src/app/feature/admin-panel/admin-panel.component.ts
+++ src/app/feature/admin-panel/admin-panel.component.ts
@@ -11,6 +11,8 @@ import { NavModel } from "../../models/nav.model";
 import { DatabaseService } from "../../services/database.service";
 import { ToastRef, ToastrService } from "ngx-toastr";
 import { IClient } from "../../interfaces/IClient.interface";
+import { IContext } from "mocha";
+import { IAddScheme } from "../../interfaces/IAddScheme.interface";
 @Component({
   selector: "app-admin-panel",
   templateUrl: "./admin-panel.component.html",
@@ -18,15 +20,13 @@ import { IClient } from "../../interfaces/IClient.interface";
 })
 export class AdminPanelComponent implements OnInit {
   passwordForm: FormGroup;
-  clientForm: FormGroup;
-  schemeForm: FormGroup;
+
   isAuthenticated: boolean;
-  collectionArr = ["Mutual Fund", "SIP", "Equity"];
-  freqType = ["Monthly", "Yearly", "Quaterly"];
+
   navData: Array<NavModel>;
 
   mFundAndSchemeMapping = {};
-  schemeNameArr: Array<string>;
+
   selectedCollection: string;
 
   constructor(
@@ -37,9 +37,6 @@ export class AdminPanelComponent implements OnInit {
 
   ngOnInit(): void {
     this.isAuthenticated = false;
-    this.passwordForm = new FormGroup({
-      password: new FormControl(1234, [Validators.required]),
-    });
   }
   getNavData() {
     this.navModelService
@@ -77,82 +74,18 @@ export class AdminPanelComponent implements OnInit {
 
     return schemeNamesArr.sort();
   }
-  generateClientAndSchemeForm() {
-    this.clientForm = new FormGroup({
-      name: new FormControl("Abhijeet", [Validators.required]),
-      folioNumber: new FormControl(1234, [Validators.required]),
-      regDate: new FormControl(null, [Validators.required]),
-      collection: new FormControl("SIP", [Validators.required]),
-    });
-    this.schemeForm = new FormGroup({
-      schemeName: new FormControl(null, [Validators.required]),
-      schemeCode: new FormControl({ value: null, disabled: true }, [
-        Validators.required,
-      ]),
-      folioNo: new FormControl(null, [Validators.required]),
-      startDate: new FormControl({ year: 2020, month: 6, day: 6 }, [
-        Validators.required,
-      ]),
-      freqType: new FormControl({ value: "Monthly", disabled: true }, [
-        Validators.required,
-      ]),
-      mFundFamily: new FormControl(null, [Validators.required]),
-      amt: new FormControl(null, [Validators.required]),
-      nav: new FormControl(null, [Validators.required]),
-      units: new FormControl({ value: null, disabled: true }, [
-        Validators.required,
-      ]),
-    });
-    this.triggerValueChanges();
-  }
-  triggerValueChanges() {
-    this.schemeForm
-      .get("schemeName")
-      .valueChanges.subscribe((selectedSchemeName: string) => {
-        if (selectedSchemeName) {
-          const item: NavModel = this.navData.filter(
-            (item: NavModel) =>
-              item.schemeName.toLowerCase() == selectedSchemeName.toLowerCase()
-          )[0];
 
-          this.schemeForm.get("schemeCode").setValue(item.schemeCode);
-        }
-      });
-    this.schemeForm
-      .get("mFundFamily")
-      .valueChanges.subscribe((selectedFundName: string) => {
-        this.schemeNameArr = this.mFundAndSchemeMapping[
-          selectedFundName
-        ].sort();
-        this.schemeForm.get("schemeName").setValue(null);
-        this.schemeForm.get("schemeCode").setValue(null);
-      });
-
-    this.schemeForm.get("amt").valueChanges.subscribe((amt) => {
-      const nav = this.schemeForm.get("nav").value;
-      const units = this.calculatedUnits(amt, nav);
-      this.schemeForm.get("units").setValue(units);
-    });
-    this.schemeForm.get("nav").valueChanges.subscribe((nav) => {
-      const amt = this.schemeForm.get("amt").value;
-      const units = this.calculatedUnits(amt, nav);
-      this.schemeForm.get("units").setValue(units);
-    });
-  }
-  onPwdSubmit() {
-    const pwd = this.passwordForm.get("password").value;
+  getPwd(pwd: string) {
     if (pwd == "1234") {
       this.isAuthenticated = true;
-      this.generateClientAndSchemeForm();
+
       this.getNavData();
     }
   }
-  onClientInfoSubmit() {
-    const client: IClient = {
-      name: this.clientForm.get("name").value,
-      folioNo: this.clientForm.get("folioNumber").value,
-      isActive: true,
-    };
+  getSchemeDataToInsert(schemeData: IAddScheme) {
+    console.log("schemeData ", schemeData);
+  }
+  getClientToInsert(client: IClient) {
     this.dbService
       .addClient(client)
       .then((res) => {
@@ -167,17 +100,5 @@ export class AdminPanelComponent implements OnInit {
           "Something went wrong! Couldn't add the client. Try again!"
         );
       });
-  }
-  onSchemeFormSubmit() {
-    console.log(this.schemeForm.getRawValue());
-    this.selectedCollection = this.schemeForm.get("collection").value;
-  }
-  calculatedUnits(amt: number, nav: number) {
-    let units = null;
-    if (amt && nav) {
-      units = amt / nav;
-      units = Math.round(units * 10) / 10;
-    }
-    return units;
   }
 }
