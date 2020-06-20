38bb62b7329194834deec0fcddda99050bdc625b
Abhijeet <abhijeetlokhande1996@gmail.com>
2020-06-20 20:48:56 +0530

    global filter done

--- src/app/shared/prime-table/prime-table.component.ts
+++ src/app/shared/prime-table/prime-table.component.ts
@@ -8,8 +8,18 @@ import { SipInterface } from "../../interfaces/sip.interface";
 })
 export class PrimeTableComponent implements OnInit {
   _dataToShow: Array<any>;
-
-  @Input() colHeaderMapArray;
+  filterFields: Array<string>;
+  _colHeaderMapArray: [];
+  @Input()
+  set colHeaderMapArray(val) {
+    if (val) {
+      this.filterFields = [];
+      this._colHeaderMapArray = val;
+      for (const header of this._colHeaderMapArray) {
+        this.filterFields.push(header[0]);
+      }
+    }
+  }
   @Input()
   set sipDataToShow(data: Array<any>) {
     this._dataToShow = data;
